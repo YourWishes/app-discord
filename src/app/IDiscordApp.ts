5c9fb84eceedf1a0dc70a752a197c84e2ef50172
Dominic Masters <dominic@domsplace.com>
2018-12-24 05:10:11 +1100

    Fixed missing typescript reference and recompiled.

--- src/app/IDiscordApp.ts
+++ src/app/IDiscordApp.ts
@@ -22,6 +22,7 @@
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 import { IApp } from '@yourwishes/app-base';
+import { DiscordModule } from './../discord/';
 
 export interface IDiscordApp extends IApp {
   discord:DiscordModule;
